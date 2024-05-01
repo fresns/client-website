21b703f62114552e64be8773486c758bec98f892
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-05-01 08:33:58 +0800

    chore: notification types

--- Moments/messages/notifications.blade.php
+++ Moments/messages/notifications.blade.php
@@ -7,7 +7,7 @@
         <h1 class="fs-5">{{ fs_config('channel_notifications_name') }}</h1>
 
         {{-- all notifications --}}
-        <a class="nav-link rounded-pill px-3 pt-1" style="@empty($types) background-color: var(--bs-orange);color: #FFF; @endempty" href="{{ route('fresns.notification.index') }}">
+        <a class="nav-link rounded-pill px-3 pt-1" style="@empty(request('types')) background-color: var(--bs-orange);color: #FFF; @endempty" href="{{ route('fresns.notification.index') }}">
             {{ fs_config('channel_notifications_all_name') }}
         </a>
     </div>
@@ -16,7 +16,7 @@
     <nav class="nav nav-pills nav-fill nav-justified gap-2 p-1 small bg-white border rounded-pill shadow-sm m-3">
         {{-- system notifications --}}
         @if (in_array('systems', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 1) active @endif" href="{{ route('fresns.notification.index', ['types' => 1]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 1) active @endif" href="{{ route('fresns.notification.index', ['types' => 1]) }}">
                 {{ fs_config('channel_notifications_systems_name') }}
                 @if (fs_user_overview('unreadNotifications.systems') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.systems') }}</span>
@@ -26,7 +26,7 @@
 
         {{-- recommend notifications --}}
         @if (in_array('recommends', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 2) active @endif" href="{{ route('fresns.notification.index', ['types' => 2]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 2) active @endif" href="{{ route('fresns.notification.index', ['types' => 2]) }}">
                 {{ fs_config('channel_notifications_recommends_name') }}
                 @if (fs_user_overview('unreadNotifications.recommends') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.recommends') }}</span>
@@ -36,7 +36,7 @@
 
         {{-- like notifications --}}
         @if (in_array('likes', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 3) active @endif" href="{{ route('fresns.notification.index', ['types' => 3]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 3) active @endif" href="{{ route('fresns.notification.index', ['types' => 3]) }}">
                 {{ fs_config('channel_notifications_likes_name') }}
                 @if (fs_user_overview('unreadNotifications.likes') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.likes') }}</span>
@@ -46,7 +46,7 @@
 
         {{-- dislike notifications --}}
         @if (in_array('dislikes', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 4) active @endif" href="{{ route('fresns.notification.index', ['types' => 4]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 4) active @endif" href="{{ route('fresns.notification.index', ['types' => 4]) }}">
                 {{ fs_config('channel_notifications_dislikes_name') }}
                 @if (fs_user_overview('unreadNotifications.dislikes') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.dislikes') }}</span>
@@ -56,7 +56,7 @@
 
         {{-- follow notifications --}}
         @if (in_array('follows', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 5) active @endif" href="{{ route('fresns.notification.index', ['types' => 5]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 5) active @endif" href="{{ route('fresns.notification.index', ['types' => 5]) }}">
                 {{ fs_config('channel_notifications_follows_name') }}
                 @if (fs_user_overview('unreadNotifications.follows') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.follows') }}</span>
@@ -66,7 +66,7 @@
 
         {{-- block notifications --}}
         @if (in_array('blocks', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 6) active @endif" href="{{ route('fresns.notification.index', ['types' => 6]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 6) active @endif" href="{{ route('fresns.notification.index', ['types' => 6]) }}">
                 {{ fs_config('channel_notifications_blocks_name') }}
                 @if (fs_user_overview('unreadNotifications.blocks') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.blocks') }}</span>
@@ -76,7 +76,7 @@
 
         {{-- mention notifications --}}
         @if (in_array('mentions', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 7) active @endif" href="{{ route('fresns.notification.index', ['types' => 7]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 7) active @endif" href="{{ route('fresns.notification.index', ['types' => 7]) }}">
                 {{ fs_config('channel_notifications_mentions_name') }}
                 @if (fs_user_overview('unreadNotifications.mentions') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.mentions') }}</span>
@@ -86,7 +86,7 @@
 
         {{-- comment notifications --}}
         @if (in_array('comments', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 8) active @endif" href="{{ route('fresns.notification.index', ['types' => 8]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 8) active @endif" href="{{ route('fresns.notification.index', ['types' => 8]) }}">
                 {{ fs_config('channel_notifications_comments_name') }}
                 @if (fs_user_overview('unreadNotifications.comments') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.comments') }}</span>
@@ -96,7 +96,7 @@
 
         {{-- quote notifications --}}
         @if (in_array('quotes', fs_config('moments_notifications', [])))
-            <a class="nav-link rounded-pill px-2 @if ($types == 9) active @endif" href="{{ route('fresns.notification.index', ['types' => 9]) }}">
+            <a class="nav-link rounded-pill px-2 @if (request('types') == 9) active @endif" href="{{ route('fresns.notification.index', ['types' => 9]) }}">
                 {{ fs_config('channel_notifications_quotes_name') }}
                 @if (fs_user_overview('unreadNotifications.quotes') > 0)
                     <span class="badge bg-danger">{{ fs_user_overview('unreadNotifications.quotes') }}</span>
@@ -112,11 +112,11 @@
         </div>
     @else
         {{-- Read Button --}}
-        @if ($types)
+        @if (request('types'))
             <div class="border-bottom text-center py-3">
                 <form class="api-request-form" action="{{ route('fresns.api.patch', ['path' => '/api/fresns/v1/notification/read-status']) }}" method="patch">
                     <input type="hidden" name="type" value="all"/>
-                    <input type="hidden" name="notificationType" value="{{ $types }}"/>
+                    <input type="hidden" name="notificationType" value="{{ request('types') }}"/>
                     <input type="hidden" name="notificationIds" value=""/>
                     <button class="btn btn-success btn-sm" type="submit">{{ fs_lang('notificationMarkAllAsRead') }}</button>
                 </form>
